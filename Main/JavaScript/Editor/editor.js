11f353ed1dcce20de451394220a71d67d4c8dc34
jonicoke <cokejonathang@gmail.com>
2025-10-16 00:13:03 -0300

    agrego comentarios descriptivos para cada funcion y cambio en editor.html y editor.js. Función hecha para cargar una imagen

--- Main/JavaScript/Editor/editor.js
+++ Main/JavaScript/Editor/editor.js
@@ -1,2 +1,36 @@
-inputImagen = document.getElementById("input-imagen")
+// Creo el objeto Canvas en el elemento <canvas> de id #canvas-imagen
+var canvas = new fabric.Canvas("canvas-image");
 
+// Guardo el elemento input de id #input-imagen en una constante
+const inputImagen = document.getElementById("input-imagen");
+
+// escucho el evento change en inputImagen 
+// y hago la funcion para el elemento (input)
+inputImagen.addEventListener("change", function (elemento) {
+//  guardo en una constante el primer archivo del elemento input
+    const archivo = elemento.target.files[0]; 
+  
+    if (!archivo) return; // si no se seleccionó ninguna imagen, no hay primer elemento [0] --> entonces cierro el programa
+
+  // Si hay elemento -> leo el archivo como URL para usarlo en una etiqueta <img>
+  const lector = new FileReader();
+
+// Hago un evento de descarga para guardar la imagen que tengo como URL con fabric js
+  lector.onload = function (event) {
+//  Uso la funcion que tiene fabric .Imagen.fromURL y al elemento que llamó el evento, entro a su target.result 
+//  En el console.log se vio que ahí está la URL
+fabric.Image.fromURL(event.target.result, function (img) {
+    // ajusto tamaño
+    img.scaleToWidth(200); 
+    img.scaleToHeight(200);
+    // limpio el canvas anterior
+      canvas.clear(); 
+    //añado al canvas la imagen subida
+      canvas.add(img);
+      canvas.centerObject(img);
+      canvas.renderAll();
+    });
+  };
+  // esta linea imprime la imagen en pantalla leyendo el DATAURL y codificandolo a base64
+  lector.readAsDataURL(archivo); 
+});
